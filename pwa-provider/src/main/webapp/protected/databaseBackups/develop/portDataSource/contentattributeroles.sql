cae1c73cf563f6f2d8b0f8b56b5ec55810b358a0
joewhite101 <j.white@entando.com>
2019-03-18 14:28:06 +0100

    Remove placeholder content

--- pwa-provider/src/main/webapp/protected/databaseBackups/develop/portDataSource/contentattributeroles.sql
+++ pwa-provider/src/main/webapp/protected/databaseBackups/develop/portDataSource/contentattributeroles.sql
@@ -1,11 +1,5 @@
 INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('CNG2','Title','jacms:title');
 INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('CNG3','Title','jacms:title');
-INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS4','Title','jacms:title');
-INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS5','Title','jacms:title');
-INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS6','Title','jacms:title');
-INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS7','Title','jacms:title');
-INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS8','Title','jacms:title');
-INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS9','Title','jacms:title');
 INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS10','Title','jacms:title');
 INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS13','Title','jacms:title');
 INSERT INTO contentattributeroles (contentid,attrname,rolename) VALUES ('NWS16','Title','jacms:title');
--- pwa-provider/src/main/webapp/protected/databaseBackups/develop/portDataSource/contentrelations.sql
+++ pwa-provider/src/main/webapp/protected/databaseBackups/develop/portDataSource/contentrelations.sql
@@ -2,12 +2,6 @@ INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcatego
 INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('CNG2',NULL,NULL,'sampleimgone',NULL,NULL);
 INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('CNG3',NULL,NULL,NULL,NULL,'free');
 INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('CNG3',NULL,NULL,'sampleimgone',NULL,NULL);
-INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS4',NULL,NULL,NULL,NULL,'free');
-INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS5',NULL,NULL,NULL,NULL,'free');
-INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS6',NULL,NULL,NULL,NULL,'free');
-INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS7',NULL,NULL,NULL,NULL,'free');
-INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS8',NULL,NULL,NULL,NULL,'free');
-INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS9',NULL,NULL,NULL,NULL,'free');
 INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS10',NULL,NULL,NULL,'amministrazione',NULL);
 INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS10',NULL,NULL,NULL,NULL,'free');
 INSERT INTO contentrelations (contentid,refpage,refcontent,refresource,refcategory,refgroup) VALUES ('NWS10',NULL,NULL,'11',NULL,NULL);
