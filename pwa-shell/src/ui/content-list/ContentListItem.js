38aed9368c5177950b2b059cc747f4e43c5be49a
Giuseppe de Santis <g.desantis@entando.com>
2019-04-11 11:17:11 +0200

    fix special characters in notification content mocks fix word breaking in content list items little style cleanups and fixes

--- pwa-shell/src/ui/content-list/ContentListItem.js
+++ pwa-shell/src/ui/content-list/ContentListItem.js
@@ -10,26 +10,20 @@ import Badge from 'ui/common/Badge';
 
 const ContentListItem = ({ data }) => (
   <Card className="shadow ContentListItem">
-    <CardHeader>
+    <CardHeader className="ContentListItem__header">
       {
         data.isUnread
           ? <Badge className="ContentListItem__status--unread">nuovo</Badge>
           : ''
       }
     </CardHeader>
-    <CardBody>
-      <Link to={`/content/${data.typeCode}/${data.id}${data.requiresAuth ? '?requiresAuth=true' : ''}`}>
-        <div dangerouslySetInnerHTML={{__html: data.html}}>
-        </div>
-        <div>
-          <span className="item-footer">
-          </span>
-        </div>
+    <CardBody className="ContentListItem__body">
+      <Link className="ContentListItem__link-to-detail" to={`/content/${data.typeCode}/${data.id}${data.requiresAuth ? '?requiresAuth=true' : ''}`}>
+        <div dangerouslySetInnerHTML={{__html: data.html}} />
       </Link>
     </CardBody>
   </Card>
 );
-
 
 ContentListItem.propTypes = {
   data: PropTypes.shape({
