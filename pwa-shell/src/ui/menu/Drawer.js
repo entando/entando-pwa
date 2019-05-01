8b15bb58b8eb270e0724ea7d9e0211fc4073df64
Jeff Go <j.go@entando.com>
2019-05-01 10:46:23 +0800

    SME-12 - react-use-gesture added and added swipe gesture for drawer closing

--- pwa-shell/src/ui/menu/Drawer.js
+++ pwa-shell/src/ui/menu/Drawer.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Gesture } from 'react-with-gesture';
 
 import { ReactComponent as Close } from 'images/icons/ic_close.svg';
 import LogoutContainer from 'ui/menu/LogoutContainer';
@@ -11,6 +12,7 @@ class Drawer extends Component
 
     this.setWrapperRef = this.setWrapperRef.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleTouchEnd = this.handleTouchEnd.bind(this);
   }
 
   componentDidMount() {
@@ -42,20 +44,33 @@ class Drawer extends Component
       this.props.closeDrawer();
     }
   }
+  
+  handleTouchEnd(event) {
+    if (event.velocity >= 0.15 && event.delta[0] < 0) this.props.closeDrawer(); 
+  }
 
   render() {
-    const state = this.props.isOpen ? 'open' : 'closed';
-
+    const { isOpen } = this.props;
+    const state = isOpen ? 'open' : 'closed';
+    const config = { event: { passive: false } };
     return (
       <Fragment>
         <div className={`grey-overlay ${state}`}></div>
-        <div ref={this.setWrapperRef} className={`drawer vh-100 shadow ${state}`}>
-          <div className="p-2">
-            <Close onClick={this.props.closeDrawer} className="cursor-pointer color-primary-lightest float-right" />
-          </div>
-          <LogoutContainer />
-          {this.props.children}
-        </div>
+        <Gesture {...config} onUp={this.handleTouchEnd}>
+          {event => {
+            const { down, delta } = event;
+            return (
+              <div ref={this.setWrapperRef} className={`drawer vh-100 shadow ${state}`} style={(isOpen && down && delta[0] < 0) ? { transition: 'none', left: delta[0] } : {}}>
+                <div className="p-2">
+                  <Close onClick={this.props.closeDrawer} className="cursor-pointer color-primary-lightest float-right" />
+                </div>
+                <LogoutContainer />
+                {this.props.children}
+              </div>
+            );
+            }
+          }
+        </Gesture>
       </Fragment>
     );
   }
