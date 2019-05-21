8c178cc644930faa0a133a35292a1028bac905d4
Giuseppe de Santis <g.desantis@entando.com>
2019-05-21 15:57:38 +0200

    SME-39 restore old login (still keeping all the "requiresAuth" logic)

--- pwa-shell/src/ui/content-detail/ContentDetail.js
+++ pwa-shell/src/ui/content-detail/ContentDetail.js
@@ -2,7 +2,6 @@ import { get } from 'lodash';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from 'reactstrap';
-import ProtectedContentLoginContainer from 'ui/login/ProtectedContentLoginContainer';
 import Page from 'ui/common/Page';
 import SwipeContentNavigator from 'ui/common/SwipeContentNavigator';
 import ItemCategoryListContainer from 'ui/common/ItemCategoryListContainer';
@@ -11,7 +10,7 @@ class ContentDetail extends PureComponent {
   state = {
     nextURL: '',
     previousURL: '',
-  }
+  };
 
   fetchDetail() {
     const { location, match } = this.props;
@@ -24,25 +23,35 @@ class ContentDetail extends PureComponent {
 
   componentDidUpdate(prevProps) {
     const newParams = get(this.props, 'match.params');
-    if (this.props.isUserLogged !== prevProps.isUserLogged || (newParams && newParams !== prevProps.match.params)) {
+    if (
+      this.props.isUserLogged !== prevProps.isUserLogged ||
+      (newParams && newParams !== prevProps.match.params)
+    ) {
       this.fetchDetail();
-    } else if (this.props.nextContent !== prevProps.nextContent || this.props.prevContent !== prevProps.prevContent) {
+    } else if (
+      this.props.nextContent !== prevProps.nextContent ||
+      this.props.prevContent !== prevProps.prevContent
+    ) {
       this.checkContentSiblings();
     }
   }
 
   checkContentSiblings() {
-    const {
-      nextContent,
-      prevContent,
-      contentType,
-    } = this.props;
+    const { nextContent, prevContent, contentType } = this.props;
 
     const hasNext = Object.keys(nextContent).length > 0;
     const hasPrev = Object.keys(prevContent).length > 0;
     this.setState({
-      nextURL: hasNext ? `/content/${contentType}/${nextContent.id}${nextContent.requiresAuth ? '?requiresAuth=true' : ''}` : '',
-      previousURL: hasPrev ? `/content/${contentType}/${prevContent.id}${prevContent.requiresAuth ? '?requiresAuth=true' : ''}` : '',
+      nextURL: hasNext
+        ? `/content/${contentType}/${nextContent.id}${
+            nextContent.requiresAuth ? '?requiresAuth=true' : ''
+          }`
+        : '',
+      previousURL: hasPrev
+        ? `/content/${contentType}/${prevContent.id}${
+            prevContent.requiresAuth ? '?requiresAuth=true' : ''
+          }`
+        : '',
     });
   }
 
@@ -54,25 +63,22 @@ class ContentDetail extends PureComponent {
     const contentDetailBody = !isLoading ? (
       <Container fluid>
         <ItemCategoryListContainer categoryIdList={associatedCategoryIdList} />
-        <div dangerouslySetInnerHTML={{__html: get(contentDetail, 'html', '')}}></div>
-      </Container>  
+        <div
+          dangerouslySetInnerHTML={{ __html: get(contentDetail, 'html', '') }}
+        />
+      </Container>
     ) : (
-      <div className="mt-4">
-        { loadingMessage }
-      </div>
+      <div className="mt-4">{loadingMessage}</div>
     );
-        
+
     return (
-      <Page
-        className={`ContentDetail${isUserLogged ? '' : '--guest-user'}`}
-      > 
-        <ProtectedContentLoginContainer>
-          <SwipeContentNavigator nextURL={this.state.nextURL} previousURL={this.state.previousURL}>
-            <div className="ContentDetail__body">
-              { contentDetailBody }
-            </div>
-          </SwipeContentNavigator>
-        </ProtectedContentLoginContainer>
+      <Page className={`ContentDetail${isUserLogged ? '' : '--guest-user'}`}>
+        <SwipeContentNavigator
+          nextURL={this.state.nextURL}
+          previousURL={this.state.previousURL}
+        >
+          <div className="ContentDetail__body">{contentDetailBody}</div>
+        </SwipeContentNavigator>
       </Page>
     );
   }
@@ -87,7 +93,7 @@ ContentDetail.propTypes = {
   nextContent: PropTypes.object,
 };
 
-ContentDetail.defaultProps = {  
+ContentDetail.defaultProps = {
   contentDetail: null,
   contentType: null,
   prevContent: {},
