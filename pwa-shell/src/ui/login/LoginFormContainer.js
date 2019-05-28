47cb7ece2cd5b16d9c70e5a7737726413ff0e1cd
Jeff Go <j.go@entando.com>
2019-05-28 11:22:19 +0800

    SME-29 Localization - remove passing intl param in LoginForm for thunks

--- pwa-shell/src/ui/login/LoginFormContainer.js
+++ pwa-shell/src/ui/login/LoginFormContainer.js
@@ -1,15 +1,9 @@
 import { connect } from 'react-redux';
-import { injectIntl } from 'react-intl';
 import Login from 'ui/login/LoginForm';
 import { login as performLogin } from 'state/thunks';
 
-export const mapDispatchToProps = (dispatch, { intl }) => ({
-  onSubmit: data => dispatch(performLogin(data, intl)),
+export const mapDispatchToProps = (dispatch) => ({
+  onSubmit: data => dispatch(performLogin(data)),
 });
 
-export default injectIntl(
-  connect(
-    null,
-    mapDispatchToProps,
-  )(Login),
-);
+export default connect(null, mapDispatchToProps)(Login);
