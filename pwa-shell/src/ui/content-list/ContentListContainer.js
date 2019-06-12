77d0e6ff8031b0393881c034e6b1cd05f5a2cf16
Giuseppe de Santis <g.desantis@entando.com>
2019-06-12 21:15:13 +0200

    SME-18 update ContentListContainer so that content list is fetched only once

--- pwa-shell/src/ui/content-list/ContentListContainer.js
+++ pwa-shell/src/ui/content-list/ContentListContainer.js
@@ -11,12 +11,17 @@ import {
   isLoading,
 } from 'state/content/selectors';
 import { getSearchTerms } from 'state/search/selectors';
+import { getSelectedContentType } from 'state/contentType/selectors';
 
 export const mapStateToProps = (state, ownProps) => ({
   contentList: getContentList(state),
   contentListMeta: getContentListMeta(state),
   hasMoreItems: getListHasMorePages(state),
-  contentType: get(ownProps, 'match.params.contentType'),
+  contentType: get(
+    ownProps,
+    'match.params.contentType',
+    getSelectedContentType(state),
+  ),
   selectedCategoryCodes: getSelectedCategoryFilters(state),
   isSearchResult: isSearchResult(state),
   isLoading: isLoading(state),
