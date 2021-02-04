1e3cec458e193a6a6ad571600f9b16090d48164f
GermanoGiudici <germano.giudici@gmail.com>
2021-02-04 11:15:23 +0100

    static player

--- pwa-shell/src/ui/radio/Player.js
+++ pwa-shell/src/ui/radio/Player.js
@@ -1,43 +1,11 @@
-import React, { useState, useEffect } from 'react';
-
-const useAudio = url => {
-  const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
-
-  const toggle = () => setPlaying(!playing);
-
-  useEffect(
-    () => {
-      playing ? audio.play() : audio.pause();
-    },
-    // eslint-disable-next-line
-    [playing],
-  );
-
-  useEffect(() => {
-    audio.addEventListener('ended', () => setPlaying(false));
-    return () => {
-      audio.removeEventListener('ended', () => setPlaying(false));
-    };
-    // eslint-disable-next-line
-  }, []);
-
-  return [playing, toggle];
-};
+import React from 'react';
 
 const Player = ({ url, imgPlay, imgStop }) => {
-  const [playing, toggle] = useAudio(url);
-
-  const img = playing ? imgStop : imgPlay;
-
   return (
     <div className="Player">
-      <img
-        className="Player__logo"
-        src={img}
-        alt={'radio esercito'}
-        onClick={toggle}
-      />
+      <a href={url} target={'_blank'}>
+        <img className="Player__logo" src={imgPlay} alt={'radio esercito'} />
+      </a>
     </div>
   );
 };
